374129273c3040d002534337669e9e9aa5ee9b4d
Cody Milliron <codymilliron.dev@gmail.com>
2025-11-09 18:19:13 -0500

    Print sorted output

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from stats import count_words, number_of_letters
+from stats import count_words, number_of_letters, sort_letter_counts
 
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
+
+
+def print_structured_count(letter_count_list):
+    print("--------- Character Count -------")
+    for item in letter_count_list:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
 
 
 def main():
@@ -14,7 +21,8 @@ def main():
     num_words = count_words(book_content)
     print(f"Found {num_words} total words")
     letter_counts = number_of_letters(book_content)
-    print(letter_counts)
+    processed_letter_counts = sort_letter_counts(letter_counts)
+    print_structured_count(processed_letter_counts)
     
 
     
--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+def sort_on(item):
+    return item['count']
+
 def count_words(book_content):
     word_list = book_content.split()
     return len(word_list)
@@ -11,3 +14,14 @@ def number_of_letters(book_content):
         except KeyError:
             count_dict[character.lower()] = 1
     return count_dict
+
+
+def sort_letter_counts(letter_counts):
+    letter_list = []
+    for letter in letter_counts:
+        # print(letter)
+        if letter.isalpha():
+            letter_list.append({"char": letter, "count": letter_counts[letter]})
+    # print(letter_list)
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
